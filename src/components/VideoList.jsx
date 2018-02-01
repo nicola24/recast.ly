51c37a57e7ecc3275d9b530c8ddfcabebbd481be
nicola24 <nicolas.peyrichou@gmail.com>
2018-01-31 17:33:57 -0800

    pushing updates to recastly repo

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,11 @@
-var VideoList = () => (
+var VideoList = (props) => (
+  
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><VideoListEntry video={props.videos[0]}/></div>
   </div>
 );
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
